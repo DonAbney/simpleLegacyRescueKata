694d0501b9cf31a99a412c086be9934c38b282fd
mhouse89 <matthew.house89@gmail.com>
2016-01-29 17:13:01 -0500

    added skeleton for ruby version of simpleLegacyRescue kata.

---
+++ ruby/spec/some_class_spec.rb
@@ -0,0 +1,14 @@
+require_relative '../src/some_class'
+
+describe SomeClass do
+  describe '.someMethod' do
+
+    context 'does stuff' do
+      it 'returns stuff' do
+
+        expect(SomeClass.someMethod).to eql(1)
+      end
+    end
+
+  end
+end
---
+++ ruby/src/some_class.rb
@@ -0,0 +1,6 @@
+class SomeClass
+  def self.someMethod
+    0
+
+  end
+end
